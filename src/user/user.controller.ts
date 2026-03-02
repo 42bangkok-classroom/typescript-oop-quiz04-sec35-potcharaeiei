b0dd8ec841f31afe18cd7093bf3e98c8ba7d9e38
potcharaeiei <68010736@kmitl.ac.th>
2026-03-02 10:18:46 +0700

    02

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
-import { IUser } from './user.interface';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('test')
   test(): any[] {
     return this.userService.test();
-  }
-
-  @Get()
-  async findAll(): Promise<IUser[]> {
-    return this.userService.findAll();
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { readFile } from 'fs/promises';
-import { IUser } from './user.interface';
 
 @Injectable()
 export class UserService {
   test(): any[] {
     return [];
-  }
-
-  async findAll(): Promise<IUser[]> {
-    const data = await readFile('data/users.json', 'utf-8');
-    return JSON.parse(data) as IUser[];
   }
 }
