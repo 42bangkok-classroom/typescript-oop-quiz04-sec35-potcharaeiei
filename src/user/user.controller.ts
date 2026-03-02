3248008dc1166e20f8973fb3868da9981dbeefe4
potcharaeiei <68010736@kmitl.ac.th>
2026-03-02 10:14:26 +0700

    03

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { IUser } from './user.interface';
 
 @Controller('users')
 export class UserController {
@@ -8,5 +9,10 @@ export class UserController {
   @Get('test')
   test(): any[] {
     return this.userService.test();
+  }
+
+  @Get()
+  async findAll(): Promise<IUser[]> {
+    return this.userService.findAll();
   }
 }
