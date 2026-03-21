acdf3fd7ccd4dcf97b5cf7d10a375620056ef576
potcharaeiei <68010736@kmitl.ac.th>
2026-03-22 03:13:06 +0700

    try

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get , Param , Query } from '@nestjs/common';
 import { UserService } from './user.service';
 
-@Controller('users')
+@Controller('users')//P03
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('test')
+  @Get('test')//P02
   test(): any[] {
     return this.userService.test();
   }
 
-  @Get()
+  @Get()//P03
   findAll() {
     return this.userService.findAll();
+  }
+  @Get(':id')
+  findOne(
+    @Param('id') id: string,
+    @Query('fields') fields?: string,
+  ) {
+    const fieldArray = fields ? fields.split(',') : undefined;
+
+    return this.userService.findOne(id, fieldArray);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -15,6 +15,27 @@ export class UserService {
     const data = readFileSync(filePath, 'utf-8');
     this.users = JSON.parse(data) as IUser[];
   }
+  findOne(id: string,fields?:string[]): Partial<IUser> { 
+    const user = this.users.find((u) => u.id === id);
+    //ถ้าหาไอดีไม่เจอให้โยน Error ออกมา
+    if (!user) {
+      throw new Error('User not found');
+    }
+    //ถ้าไม่มีการระบุ fields หรือ fields เป็น array ว่าง ให้ return user ทั้งหมด
+    if (!fields || fields.length === 0) {
+      return user;
+    }
+    //ถ้ามีการระบุ fields ให้ return เฉพาะ field ที่ระบุ
+    const result: Partial<IUser> = {};
+    //วนลูป fields ที่ระบุมา แล้วเช็คว่า field นั้นมีอยู่ใน user หรือไม่ ถ้ามีให้เพิ่มเข้าไปใน result
+    fields.forEach((field) => {
+      if (field in user) {
+        result[field] = user[field as keyof IUser];
+      } 
+  });   
+    return result;
+  }
+  
   findAll(): IUser[] {
     return this.users;
   }
