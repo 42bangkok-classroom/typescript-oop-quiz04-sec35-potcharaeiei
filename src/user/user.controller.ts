9659c9ad2681bbb9568e2398e0d1993a1a50afbc
potcharaeiei <68010736@kmitl.ac.th>
2026-03-22 20:16:34 +0700

    Practice

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,26 +1,22 @@
-import { Controller, Get , Param , Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 
-@Controller('users')//P03
+@Controller('users') //P03
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('test')//P02
+  @Get('test') //P02
   test(): any[] {
     return this.userService.test();
   }
 
-  @Get()//P03
+  @Get() //P03
   findAll() {
     return this.userService.findAll();
   }
   @Get(':id')
-  findOne(
-    @Param('id') id: string,
-    @Query('fields') fields?: string,
-  ) {
+  findOne(@Param('id') id: string, @Query('fields') fields?: string) {
     const fieldArray = fields ? fields.split(',') : undefined;
-
     return this.userService.findOne(id, fieldArray);
   }
 }
